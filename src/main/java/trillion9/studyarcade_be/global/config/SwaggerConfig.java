64bcc31d4f73279d10afe2d8665304ca213437d1
seongwop <psw921@gmail.com>
2023-05-24 15:02:08 +0900

    Feat : Swagger 설정

---
+++ src/main/java/trillion9/studyarcade_be/global/config/SwaggerConfig.java
@@ -0,0 +1,49 @@
+package trillion9.studyarcade_be.global.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import trillion9.studyarcade_be.global.jwt.JwtUtil;
+
+@OpenAPIDefinition(
+        info = @Info(title = "StudyArcade API 명세서",
+                description = "StudyArcade API 명세서",
+                version = "v1"))
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        io.swagger.v3.oas.models.info.Info info = new io.swagger.v3.oas.models.info.Info()
+                .version("v1.0.0")
+                .title("StudyArcade")
+                .description("Api Description");
+
+        String access_token_header = JwtUtil.ACCESS_TOKEN;
+        String refresh_token_header = JwtUtil.REFRESH_TOKEN;
+
+        // 헤더에 security scheme 도 같이 보내게 만드는 것
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(access_token_header).addList(refresh_token_header);
+
+        Components components = new Components()
+                .addSecuritySchemes(access_token_header, new SecurityScheme()
+                        .name(access_token_header)
+                        .type(SecurityScheme.Type.APIKEY)
+                        .in(SecurityScheme.In.HEADER))
+                .addSecuritySchemes(refresh_token_header, new SecurityScheme()
+                        .name(refresh_token_header)
+                        .type(SecurityScheme.Type.APIKEY)
+                        .in(SecurityScheme.In.HEADER));
+
+        return new OpenAPI()
+                .info(info)
+                .addSecurityItem(securityRequirement)
+                .components(components);
+    }
+
+}
