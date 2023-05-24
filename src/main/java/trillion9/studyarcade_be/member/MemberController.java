33c7cefda4e74cb14f78064a277d771bc237ef1f
seongwop <psw921@gmail.com>
2023-05-24 15:01:45 +0900

    Feat : Swagger 설정

--- src/main/java/trillion9/studyarcade_be/member/MemberController.java
+++ src/main/java/trillion9/studyarcade_be/member/MemberController.java
@@ -1,5 +1,9 @@
 package trillion9.studyarcade_be.member;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,15 +14,28 @@ import trillion9.studyarcade_be.member.dto.MemberRequestDto;
 
 import javax.validation.Valid;
 
+@Tag(name = "MemberController", description = "회원가입/로그인 API")
 @RestController
 @RequestMapping("api/members")
 @RequiredArgsConstructor
 public class MemberController {
+
     private final MemberService memberService;
 
+    @Operation(summary = "회원가입 API", description = "회원가입")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200",description = "회원 가입 완료")})
+    // 회원가입
     @PostMapping("/register")
     public ResponseDto<String> register(@Valid @RequestBody MemberRequestDto memberRequestDto) {
         return memberService.register(memberRequestDto);
+    }
+
+    @Operation(summary = "로그인 API", description = "로그인")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200",description = "로그인 완료")})
+    // 로그인
+    @PostMapping("/login")
+    public ResponseDto<String> login(@RequestBody MemberRequestDto.login memberRequestDto){
+        return  memberService.login(memberRequestDto);
     }
 
 }
--- src/main/java/trillion9/studyarcade_be/room/RoomController.java
+++ src/main/java/trillion9/studyarcade_be/room/RoomController.java
@@ -1,5 +1,9 @@
 package trillion9.studyarcade_be.room;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -9,7 +13,7 @@ import trillion9.studyarcade_be.room.dto.RoomRequestDto;
 import trillion9.studyarcade_be.room.dto.RoomResponseDto;
 
 import java.util.List;
-
+@Tag(name = "RoomController",description = "스터디룸 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
@@ -18,6 +22,8 @@ public class RoomController {
     private final RoomService roomService;
 
     /* 스터디 룸 생성 */
+    @Operation(summary = "스터디 룸 생성 API", description = "스터디 룸 생성")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200",description = "스터디 룸 생성 완료")})
     @PostMapping("/room/create")
     public ResponseDto<RoomResponseDto> createRoom(@RequestBody RoomRequestDto requestDto,
                                                    @AuthenticationPrincipal UserDetailsImpl userDetails) {
@@ -25,6 +31,8 @@ public class RoomController {
     }
 
     /* 스터디 룸 수정 */
+    @Operation(summary = "스터디 룸 수정 API", description = "스터디 룸 수정")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200",description = "스터디 룸 수정 완료")})
     @PatchMapping("/room/{roomId}")
     public ResponseDto<RoomResponseDto> updateRoom(@PathVariable Long roomId,
                              @RequestBody RoomRequestDto requestDto,
@@ -33,6 +41,8 @@ public class RoomController {
     }
 
     /* 스터디 룸 삭제 */
+    @Operation(summary = "스터디 룸 삭제 API", description = "스터디 룸 삭제")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200",description = "스터디 룸 삭제 완료")})
     @DeleteMapping("/room/{roomId}")
     public ResponseDto<RoomResponseDto> deleteRoom(@PathVariable Long roomId,
                              @AuthenticationPrincipal UserDetailsImpl userDetails) {
@@ -40,12 +50,16 @@ public class RoomController {
     }
 
     /* 스터디 룸 목록 조회 */
+    @Operation(summary = "스터디 룸 목록 조회 API", description = "스터디 룸 목록 조회")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200",description = "스터디 룸 목록 조회 완료")})
     @GetMapping("/main")
     public ResponseDto<List<RoomResponseDto>> allRooms() {
         return roomService.allRooms();
     }
 
     /* 스터디 룸 정보 조회 */
+    @Operation(summary = "스터디 룸 정보 조회 API", description = "스터디 룸 정보 조회")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200",description = "스터디 룸 정보 조회 완료")})
     @GetMapping("/room/{roomId}")
     public ResponseDto<RoomResponseDto> infoRoom(@PathVariable Long roomId) {
         return roomService.infoRoom(roomId);
