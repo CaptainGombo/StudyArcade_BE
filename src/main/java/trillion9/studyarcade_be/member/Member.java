08013332bac3e2949f4a414c150c7abbff56997e
seongwop <psw921@gmail.com>
2023-05-24 22:42:01 +0900

    Feat : Kakao 소셜 로그인 구현

--- src/main/java/trillion9/studyarcade_be/member/Member.java
+++ src/main/java/trillion9/studyarcade_be/member/Member.java
@@ -1,5 +1,6 @@
 package trillion9.studyarcade_be.member;
 
+import lombok.Builder;
 import lombok.Getter;
 
 import javax.persistence.*;
@@ -13,10 +14,25 @@ public class Member {
     @Column(name = "member_id")
     private Long id;
 
+    private Long kakaoId;
+
     @Column(nullable = false, unique = true)
     private String nickname;
     @Column(nullable = false, unique = true)
     private String email;
     @Column(nullable = false)
     private String password;
+
+    @Builder
+    private Member(Long kakaoId, String nickname, String email, String password) {
+        this.kakaoId = kakaoId;
+        this.nickname = nickname;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Member kakaoIdUpdate(Long kakaoId) {
+        this.kakaoId = kakaoId;
+        return this;
+    }
 }
