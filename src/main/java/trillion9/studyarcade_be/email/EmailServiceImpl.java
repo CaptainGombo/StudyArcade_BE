70ddf6a4abd8c1d3f8f7b3f0ac0721ff54860f9d
seongwop <psw921@gmail.com>
2023-05-27 12:53:48 +0900

    Refactor : 이메일 인증 HTML 수정

--- src/main/java/trillion9/studyarcade_be/email/EmailServiceImpl.java
+++ src/main/java/trillion9/studyarcade_be/email/EmailServiceImpl.java
@@ -18,30 +18,19 @@ public class EmailServiceImpl implements EmailService{
 
     public static final String ePw = createKey();
 
-    private MimeMessage createMessage(String to)throws Exception{
+    private MimeMessage createMessage(String to)throws Exception {
         System.out.println("보내는 대상 : "+ to);
         System.out.println("인증 번호 : "+ ePw);
         MimeMessage  message = emailSender.createMimeMessage();
 
-        message.addRecipients(RecipientType.TO, to);//보내는 대상
-        message.setSubject("스터브 인증 메일 코드입니다.");//제목
+        message.addRecipients(RecipientType.TO, to); // 보내는 대상
+        message.setSubject("스터브 회원가입 인증 코드입니다."); // 제목
 
-        String msgg="";
-        msgg+= "<div style='margin:20px;'>";
-        msgg+= "<h1> 안녕하세요 스터브입니다. </h1>";
-        msgg+= "<br>";
-        msgg+= "<p>아래 코드를 복사해 입력해주세요<p>";
-        msgg+= "<br>";
-        msgg+= "<p>감사합니다.<p>";
-        msgg+= "<br>";
-        msgg+= "<div align='center' style='border:1px solid black; font-family:verdana';>";
-        msgg+= "<h3 style='color:blue;'>회원가입 인증 코드입니다.</h3>";
-        msgg+= "<div style='font-size:130%'>";
-        msgg+= "CODE : <strong>";
-        msgg+= ePw+"</strong><div><br/> ";
-        msgg+= "</div>";
-        message.setText(msgg, "utf-8", "html");//내용
-        message.setFrom(new InternetAddress("studyhu6@gmail.com","스터브"));//보내는 사람
+        String msgg = "";
+        msgg += "<head><title>스터브 회원가입 인증</title></head><div style='max-width: 500px; margin: 50px auto; background-color: #fff; padding: 30px; border-radius: 10px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);'><h1 style='color: #1E3C72; font-size: 32px; margin-top: 0; text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);'>안녕하세요, 스터브입니다.</h1><p style='color: #333; font-size: 18px; margin-bottom: 10px; line-height: 1.4;'>아래 코드를 복사하여 입력해주세요.</p><p style='color: #333; font-size: 18px; margin-bottom: 10px; line-height: 1.4;'>감사합니다.</p><h3 style='color: #1E3C72; font-size: 18px; margin-bottom: 15px;'>회원가입 인증 코드</h3><div><strong style='font-size: 22px; color: #0D47A1; background-color: #E1F5FE; padding: 12px 20px; border-radius: 5px; display: inline-block; text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.1);'>";
+        msgg += ePw + "</strong></div></div>";
+        message.setText(msgg, "utf-8", "html"); // 내용
+        message.setFrom(new InternetAddress("studyhu6@gmail.com","스터브")); // 보내는 사람
 
         return message;
     }
