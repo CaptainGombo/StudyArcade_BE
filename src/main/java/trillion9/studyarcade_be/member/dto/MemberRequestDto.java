d32c98e5d6c301ad9887ccd2268114dfd9499a0c
seongwop <psw921@gmail.com>
2023-05-30 14:34:28 +0900

    Refactor : 이메일 인증 처리 및 유효성 검사 리팩토링

--- src/main/java/trillion9/studyarcade_be/member/dto/MemberRequestDto.java
+++ src/main/java/trillion9/studyarcade_be/member/dto/MemberRequestDto.java
@@ -8,12 +8,16 @@ import javax.validation.constraints.Pattern;
 @Getter
 public class MemberRequestDto {
 
+    @Pattern(regexp = "^[a-zA-Z가-힣]{2,10}$", message = "닉네임은 2글자~10글자 사이의 알파벳 또는 한글로 입력해주세요.")
     private String nickname;
+
     @Email
     private String email;
-    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@$!%*?&()_])[A-Za-z\\d$@$!%*?&()_]{8,15}$", message = "비밀번호는 최소 8자 이상 15자 이하이며 알파벳 대소문자와 숫자로 구성되어야 합니다.")
+
+    @Pattern(regexp = "^(?=.[0-9])(?=.[a-z])(?=.[A-Z])(?=.\\W)(?=\\S+$).{8,15}$", message = "비밀번호는 최소 8자 이상 15자 이하이며 알파벳 대소문자와 숫자, 특수문자로 구성되어야 합니다.")
     private String password;
-    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@$!%*?&()_])[A-Za-z\\d$@$!%*?&()_]{8,15}$", message = "비밀번호는 최소 8자 이상 15자 이하이며 알파벳 대소문자와 숫자로 구성되어야 합니다.")
+
+    @Pattern(regexp = "^(?=.[0-9])(?=.[a-z])(?=.[A-Z])(?=.\\W)(?=\\S+$).{8,15}$", message = "비밀번호는 최소 8자 이상 15자 이하이며 알파벳 대소문자와 숫자, 특수문자로 구성되어야 합니다.")
     private String checkPassword;
 
     @Getter
@@ -23,5 +27,3 @@ public class MemberRequestDto {
         private String password;
     }
 }
-
-
