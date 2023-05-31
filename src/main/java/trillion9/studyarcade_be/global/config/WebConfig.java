e67ddf07b91cb537887f1248fe6020e06af93e36
seongwop <psw921@gmail.com>
2023-05-31 18:09:22 +0900

    Refactor : exposedHeaders 추가

--- src/main/java/trillion9/studyarcade_be/global/config/WebConfig.java
+++ src/main/java/trillion9/studyarcade_be/global/config/WebConfig.java
@@ -43,6 +43,6 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedHeaders("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
                 .allowedOrigins("*") // "http://localhost:3000"
-                .exposedHeaders(JwtUtil.ACCESS_TOKEN, JwtUtil.REFRESH_TOKEN);
+                .exposedHeaders(JwtUtil.ACCESS_TOKEN, JwtUtil.REFRESH_TOKEN, "Set-Cookie");
     }
 }
