d7de57b8a5bf195a80339f3fa354ee117af47881
Owner <rmsqh4617@gmail.com>
2023-06-01 21:37:39 +0900

    Fix: 방생성 에러 수정

--- src/main/java/trillion9/studyarcade_be/room/RoomController.java
+++ src/main/java/trillion9/studyarcade_be/room/RoomController.java
@@ -69,7 +69,7 @@ public class RoomController {
     @Operation(summary = "스터디 룸 삭제 API", description = "스터디 룸 삭제")
     @ApiResponses(value = {@ApiResponse(responseCode = "200",description = "스터디 룸 삭제 완료")})
     @DeleteMapping("/room/{session-id}")
-    public ResponseDto<RoomDetailResponseDto> deleteRoom(@PathVariable("session-id") String sessionId,
+    public ResponseDto<RoomDetailResponseDto> deleteRoom(@PathVariable("session-id") Long sessionId,
                              @AuthenticationPrincipal UserDetailsImpl userDetails) {
         return roomService.deleteRoom(sessionId, userDetails.getMember());
     }
