594f4f8a852e7ec769981bd7e23dda6b2aae2aa7
ãhanseunghee < zzuggumee@gmail.com>
2023-06-07 17:27:46 +0900

    Feat : 마이페이지 구현

--- src/main/java/trillion9/studyarcade_be/member/StudyTimeRepository.java
+++ src/main/java/trillion9/studyarcade_be/member/StudyTimeRepository.java
@@ -4,5 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
+
 public interface StudyTimeRepository extends JpaRepository<StudyTime, Long> {
+    List<StudyTime> findAllByMemberId(Long memberId);
+
 }
