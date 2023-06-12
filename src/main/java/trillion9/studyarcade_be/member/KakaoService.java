a438b515571a37ba33f8122ca6863d59369c6711
Owner <rmsqh4617@gmail.com>
2023-06-12 16:35:30 +0900

    Fix: 소셜로그인 회원가입 시 발급되는 임시닉네임 길이 수정 및 생성된 스터디룸이 더 이상 없을 시 페이징 예외처리 추가

--- src/main/java/trillion9/studyarcade_be/member/KakaoService.java
+++ src/main/java/trillion9/studyarcade_be/member/KakaoService.java
@@ -146,7 +146,14 @@ public class KakaoService {
                 //임시 닉네임 발급
                 String tempNickname;
                 do {
-                    tempNickname = "user" + UUID.randomUUID().toString();
+                    String ALPHA_NUMERIC_STRING = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz";
+                    StringBuilder builder = new StringBuilder();
+                    int count = 6;
+                    while (count-- != 0) {
+                        int character = (int)(Math.random() * ALPHA_NUMERIC_STRING.length());
+                        builder.append(ALPHA_NUMERIC_STRING.charAt(character));
+                    }
+                    tempNickname = "user" + builder.toString();
                 } while (memberRepository.findByNickname(tempNickname).isPresent());
 
 
