12b22cf220609339e22a5a5b0089b3713c9f4c1a
seongwop <psw921@gmail.com>
2023-06-17 15:14:58 +0900

    Refactor : mypage 통계 당일 스터디시간 반영,

--- src/main/java/trillion9/studyarcade_be/member/Member.java
+++ src/main/java/trillion9/studyarcade_be/member/Member.java
@@ -49,6 +49,10 @@ public class Member {
         this.imageUrl = imageUrl;
     }
 
+    public void setDailyStudyTime(Long dailyStudyTime) {
+        this.dailyStudyTime = dailyStudyTime;
+    }
+
     public Member kakaoIdUpdate(Long kakaoId) {
         this.kakaoId = kakaoId;
         return this;
@@ -56,12 +60,7 @@ public class Member {
 
     public void updateStudyTime(Long roomStudyTime) {
         this.dailyStudyTime += roomStudyTime;
-        updateTitle();
-    }
-
-    public void updateTotalStudyTime() {
-        this.totalStudyTime += this.dailyStudyTime;
-        this.dailyStudyTime = 0L;
+        this.totalStudyTime += roomStudyTime;
         updateTitle();
     }
 
@@ -82,7 +81,6 @@ public class Member {
             this.title = "씨앗";
         }
     }
-
 
     public void updateMember(MemberRequestDto memberRequestDto, String imageUrl) {
         this.nickname = memberRequestDto.getNickname();
