0df7ce4f82ce54f69161aa86f2e85115d26cacae
seongwop <psw921@gmail.com>
2023-06-19 03:54:20 +0900

    Feat : Sentry 적용

--- src/main/java/trillion9/studyarcade_be/global/exception/CustomException.java
+++ src/main/java/trillion9/studyarcade_be/global/exception/CustomException.java
@@ -9,4 +9,9 @@ public class CustomException extends RuntimeException {
     public CustomException(ErrorCode errorCode) {
         this.errorCode = errorCode;
     }
+
+    @Override
+    public String getMessage() {
+        return errorCode.getMessage();
+    }
 }
--- src/main/java/trillion9/studyarcade_be/global/exception/GlobalExceptionHandler.java
+++ src/main/java/trillion9/studyarcade_be/global/exception/GlobalExceptionHandler.java
@@ -1,9 +1,9 @@
 package trillion9.studyarcade_be.global.exception;
 
 import com.amazonaws.services.s3.model.AmazonS3Exception;
+import io.sentry.Sentry;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -15,21 +15,24 @@ import static trillion9.studyarcade_be.global.exception.ErrorCode.*;
 
 @RestControllerAdvice
 @Slf4j
+
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(CustomException.class)
     public ResponseEntity<ErrorResponse> handleCustomException(CustomException e) {
+        Sentry.captureException(e);
         return ErrorResponse.toResponseEntity(e.getErrorCode());
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> uncheckedError(Exception e) {
+        Sentry.captureException(e);
         return ResponseEntity.badRequest().body(e.getMessage());
     }
 
-    @ExceptionHandler({MethodArgumentNotValidException.class, HttpMessageNotReadableException.class})
-    public ResponseEntity<ErrorResponse> signValidException(MethodArgumentNotValidException exception) {
-        BindingResult bindingResult = exception.getBindingResult();
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> signValidException(MethodArgumentNotValidException e) {
+        BindingResult bindingResult = e.getBindingResult();
 
         StringBuilder builder = new StringBuilder();
 
@@ -39,19 +42,19 @@ public class GlobalExceptionHandler {
             builder.append("] ");
             builder.append(fieldError.getDefaultMessage());
         }
-
+        Sentry.captureException(e);
         return ErrorResponse.toResponseEntity(INVALID_SIGN, builder.toString());
     }
 
     @ExceptionHandler(MaxUploadSizeExceededException.class)
     protected ResponseEntity<ErrorResponse> handleMaxUploadSizeExceededException(MaxUploadSizeExceededException e) {
-        log.info("handleMaxUploadSizeExceededException", e);
-
+        Sentry.captureException(e);
         return ErrorResponse.toResponseEntity(FILE_SIZE_OVER);
     }
 
     @ExceptionHandler(AmazonS3Exception.class)
     public ResponseEntity<ErrorResponse> handleFileExtensionException(AmazonS3Exception e) {
+        Sentry.captureException(e);
         return ErrorResponse.toResponseEntity(INVALID_FILE_EXTENSION);
     }
 }
