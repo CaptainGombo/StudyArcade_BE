6764ffcd79d85c66ea631fe01f3a7693401e3b76
seongwop <psw921@gmail.com>
2023-06-20 15:28:15 +0900

    Refactor : mypage를 조회할 때마다 통계 쿼리가 수행되는 대신 자정마다 실행되는 schedular에서 통계를 Redis에 저장하고, mypage 조회 시 Redis로부터 데이터를 가져오도록 변경 (Redis 캐싱)

--- src/main/java/trillion9/studyarcade_be/room/RoomScheduler.java
+++ src/main/java/trillion9/studyarcade_be/room/RoomScheduler.java
@@ -1,6 +1,8 @@
 package trillion9.studyarcade_be.room;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,6 +16,7 @@ import trillion9.studyarcade_be.studytime.StudyTimeRepository;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Component
 @RequiredArgsConstructor
@@ -22,6 +25,7 @@ public class RoomScheduler {
     private final MemberRepository memberRepository;
     private final StudyTimeRepository studyTimeRepository;
     private final RoomMemberRepository roomMemberRepository;
+    private final RedisTemplate<String, Object> redisTemplate;
 
     @Scheduled(cron = "0 0 0 * * *") // 매일 자정에 실행되도록 설정
     @Transactional
@@ -37,7 +41,9 @@ public class RoomScheduler {
         }
         roomRepository.deleteAll(expiredRooms);
 
-        // 하루에 공부한 시간 저장
+        // 하루에 공부한 시간 저장 및 통계 저장
+        HashOperations<String, String, Long> hash = redisTemplate.opsForHash();
+
         List<Member> members =  memberRepository.findAll();
 
         for (Member member : members) {
@@ -46,11 +52,56 @@ public class RoomScheduler {
             // 자정 기준 하루 전 데이터
             LocalDate previousDate = currentDate.minusDays(1);
 
-            StudyTime dailyStudyTime = new StudyTime(member.getId(), previousDate, time);
-            studyTimeRepository.save(dailyStudyTime);
+            StudyTime previousStudyTime = new StudyTime(member.getId(), previousDate, time);
+            studyTimeRepository.save(previousStudyTime);
 
             // dailyStudyTime을 0으로 리셋
             member.setDailyStudyTime(0L);
+
+            LocalDate now = LocalDate.now();
+
+            for (int i = 0; i < 300; i++) {
+                StudyTime studyTime = new StudyTime(member.getId(), now.minusDays(i), 123L);
+                studyTimeRepository.save(studyTime);
+            }
+
+            // 마지막 7일 통계
+            List<Object[]> dailyStudyTime = studyTimeRepository.findStudyTimeByDateRange(member.getId(), now.minusDays(6), now.minusDays(1));
+            for (int i = 0; i < dailyStudyTime.size(); i++) {
+                String day = String.valueOf(dailyStudyTime.get(i)[0].toString());
+                Long studyTime = Long.parseLong(dailyStudyTime.get(i)[1].toString());
+
+                hash.put(member.getId() + "D", day, studyTime);
+            }
+            hash.put(member.getId() + "D", now.toString(), member.getDailyStudyTime());
+            redisTemplate.expire(member.getId() + "D", 1, TimeUnit.DAYS);
+
+            // 마지막 7주 통계
+            List<Object[]>  weeklyStudyTime = studyTimeRepository.findStudyTimeByWeekRange(member.getId(), now.minusWeeks(6), now);
+            for (int i = 0; i < weeklyStudyTime.size(); i++) {
+                String week = String.valueOf(weeklyStudyTime.get(i)[0].toString());
+                Long studyTime = Long.parseLong(weeklyStudyTime.get(i)[1].toString());
+                // 마지막 주에 당일 공부 시간 추가
+                if (i == weeklyStudyTime.size() - 1) {
+                    studyTime += member.getDailyStudyTime();
+                }
+                hash.put(member.getId() + "W", week, studyTime);
+            }
+            redisTemplate.expire(member.getId() + "W", 1, TimeUnit.DAYS);
+
+            // 마지막 7달 통계
+            List<Object[]>  monthlyStudyTime = studyTimeRepository.findStudyTimeByMonthRange(member.getId(), now.minusMonths(6), now);
+            for (int i = 0; i < monthlyStudyTime.size(); i++) {
+                String year = String.valueOf(monthlyStudyTime.get(i)[0].toString());
+                String month = String.valueOf(monthlyStudyTime.get(i)[1].toString());
+                Long studyTime = Long.parseLong(monthlyStudyTime.get(i)[2].toString());
+                // 마지막 달에 당일 공부 시간 추가
+                if (i == monthlyStudyTime.size() - 1) {
+                    studyTime += member.getDailyStudyTime();
+                }
+                hash.put(member.getId() + "M", year + "." + month, studyTime);
+            }
+            redisTemplate.expire(member.getId() + "M", 1, TimeUnit.DAYS);
         }
     }
 }
