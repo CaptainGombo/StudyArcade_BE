2bf318945ca4265e2b51ebe80d6338b6c2d987e4
seunghee58 <zzuggumee@gmail.com>
2023-06-24 23:48:46 +0900

    Fix : 채팅 발송 시 프로필 이미지가 null로 반환되는 에러 수정

--- src/main/java/trillion9/studyarcade_be/chat/RedisSubscriber.java
+++ src/main/java/trillion9/studyarcade_be/chat/RedisSubscriber.java
@@ -5,9 +5,12 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Service;
+import trillion9.studyarcade_be.member.Member;
+import trillion9.studyarcade_be.member.MemberRepository;
 
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Optional;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -16,6 +19,7 @@ public class RedisSubscriber {
 
 	private final ObjectMapper objectMapper;
 	private final SimpMessageSendingOperations messagingTemplate;
+	private final MemberRepository memberRepository;
 
 	/* Redis에서 메시지가 발행(publish)되면 대기하고 있던 Redis Subscriber가 해당 메시지를 받아서 처리.
 	리스너가 대기하고 있다가 메세지 오면 RedisSubscriber.sendMessage 수행 */
@@ -26,10 +30,24 @@ public class RedisSubscriber {
 			String now = LocalTime.now().format(DateTimeFormatter.ofPattern("hh:mm a"));
 			chatMessage.setCreatedAt(now);
 
+			// 프로필 이미지 설정
+			String profileImage = retrieveProfileImage(chatMessage.getNickname());
+			chatMessage.setProfileImage(profileImage);
+
 			// 스터디룸을 구독한 클라이언트에게 메시지 발송
 			messagingTemplate.convertAndSend("/sub/chat/room/" + chatMessage.getSessionId(), chatMessage);
 		} catch (Exception e) {
 			log.error("Exception {}", e);
 		}
+	}
+
+	private String retrieveProfileImage(String nickname) {
+		Optional<Member> member = memberRepository.findByNickname(nickname);
+
+		if (member.isPresent() && member.get().getImageUrl() != null) {
+			return member.get().getImageUrl();
+		}
+
+		return "대표 프로필 이미지 URL";
 	}
 }
