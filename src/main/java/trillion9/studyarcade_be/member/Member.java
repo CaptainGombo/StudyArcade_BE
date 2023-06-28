7acbc2b615dd8bffd2cb01a9d6e4da783ed009e1
seongwop <psw921@gmail.com>
2023-06-28 17:47:35 +0900

    Refactor : 코드 리팩토링

--- src/main/java/trillion9/studyarcade_be/member/Member.java
+++ src/main/java/trillion9/studyarcade_be/member/Member.java
@@ -79,7 +79,6 @@ public class Member {
         }
     }
 
-
     public void updateMember(MemberRequestDto memberRequestDto, String imageUrl, String password) {
         this.nickname = memberRequestDto.getNickname();
         this.imageUrl = imageUrl;
